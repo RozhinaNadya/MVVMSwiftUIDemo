43e841b546736112d1c20e0bcf850f389be17552
RozhinaNadya <zhuk.09@mail.ru>
2022-08-04 11:40:10 +0200

    Initial Commit

---
+++ MVVMSwiftUIDemo/MVVMSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIDemoApp.swift
+//  MVVMSwiftUIDemo
+//
+//  Created by Надежда on 2022-08-04.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
